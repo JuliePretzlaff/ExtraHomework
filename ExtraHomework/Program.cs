4438800c54cba15d4bd9f33fa15b8d917beb8c9d
JuliePretzlaff <juliapretzlaff@gmail.com>
2018-01-24 19:14:40 -0500

    Initial Commit

---
+++ ExtraHomework/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtraHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Conditionals
+
+            //Console.WriteLine("Please enter a number between 1 and 10: ");
+            //int numberEntry = int.Parse(Console.ReadLine());
+            //if(numberEntry < 10 && numberEntry > 1)
+            //{
+            //    Console.WriteLine("Valid!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid!");
+            //}
+
+            ////Next
+            // Console.WriteLine("Please enter a first number: ");
+            // int firstNum = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Please enter a second number: ");
+            // int secondNum = int.Parse(Console.ReadLine());
+            // if(firstNum > secondNum)
+            // {
+            //     Console.WriteLine("The larger number is " + firstNum);
+            // }
+            // else if(secondNum > firstNum)
+            // {
+            //     Console.WriteLine("The larger number is " + secondNum);
+            // }
+            //else
+            // {
+            //     Console.WriteLine("The numbers are equal.");
+
+            ////Next
+            //Console.WriteLine("Please enter the picture's length: ");
+            //int length = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please enter the picture's height ");
+            //int height = int.Parse(Console.ReadLine());
+            //if(height > length)
+            //{
+            //    Console.WriteLine("The picture is a portrait.");
+            //}
+            //else if(length > height)
+            //{
+            //    Console.WriteLine("The picture is a landscape.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The picture is a square.");
+
+            ////Next
+            //Console.WriteLine("Please enter the speed limit: ");
+            //int speedLimit = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please enter the speed of the car: ");
+            //int carSpeed = int.Parse(Console.ReadLine());
+            //int speedOver, dimeritNumber;
+            //if (carSpeed <= speedLimit)
+            //{
+            //    Console.WriteLine("OK");
+            //}
+            //else
+            //{
+            //    speedOver = carSpeed - speedLimit;
+            //    dimeritNumber = speedOver / 5;
+            //    if (dimeritNumber < 12)
+            //    {
+            //        Console.WriteLine("Points = " + dimeritNumber);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("License Suspended!");
+            //    }
+            //}
+
+            ////Loop practice:
+            //int sum = 0;
+            //for(int i = 1; i <=100; i++)
+            //    if(i%3 == 0)
+            //    {
+            //        sum = sum + 1;
+            //    }
+            //Console.WriteLine(sum);
+
+            //Next
+
+        }
+    }
+}
